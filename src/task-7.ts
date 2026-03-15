7583d7fa46afcbf7dc7538de70662278ad2e359e
EugenePrygaro <eugeneprygaro@gmail.com>
2026-03-15 14:16:01 +0200

    init task 1-8

---
+++ src/task-7.ts
@@ -0,0 +1,9 @@
+function getMessage() : Promise<String> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from TS");
+    }, 1000);
+  });
+}
+
+getMessage().then(result => console.log(result));
